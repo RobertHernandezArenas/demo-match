103bbedc70c54581c4764837d0661bbbf4eeb148
RobertHernandezArenas <airbusjayrobert@gmail.com>
2022-06-03 23:25:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <hls-player
+      :width="900"
+      :height="500"
+      :autoplay="true"
+      :muted="true"
+      :controls="true"
+      ref="myPlayer"
+      :source="src"
+    />
+    <h2 class="ad">
+      ---Esta retransmición es sin animo de lucro y con fines de auditoria personal---
+    </h2>
+  </div>
+</template>
+
+<script>
+import "hls-player/dist/hls-player.css";
+import hlsPlayer from "hls-player";
+
+export default {
+  name: "App",
+  components: {
+    hlsPlayer,
+  },
+  data() {
+    return {
+      src: "http://51.161.115.48:8080/live/tito_robert/ZpAzL2e7FK/93.m3u8",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
